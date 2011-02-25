33acc84213e57ff5210a7a7dc8be95e4d1cd6ad3
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-02-25 11:36:20 +0300

    Add sphinx

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -4,10 +4,10 @@
 #
 
 # Set your application name here
-appname = "appname"
+appname = "auxbonnesadresses"
 
 # Uncomment the flavor of sphinx you want to use
-#flavor = "thinking_sphinx"
+flavor = "thinking_sphinx"
 #flavor = "ultrasphinx"
 
 # If you want to install on a specific utility instance rather than
@@ -25,7 +25,7 @@ utility_name = nil
 #
 # If you don't want scheduled reindexes, just leave this set to nil.
 # Setting it equal to 10 would run the cron job every 10 minutes.
-cron_interval = nil
+cron_interval = 10
 
 if utility_name
   if ['solo', 'app', 'app_master'].include?(node[:instance_role])
