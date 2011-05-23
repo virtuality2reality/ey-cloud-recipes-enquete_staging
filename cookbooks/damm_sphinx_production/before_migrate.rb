6954ca7fe355618e4ddc4c399a21ba1cf4d85cba
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-05-23 16:34:37 +0200

    Change sphinx scripts to configure it before start or stop if configfile is not present.

--- cookbooks/damm_sphinx_production/before_migrate.rb
+++ cookbooks/damm_sphinx_production/before_migrate.rb
@@ -5,7 +5,7 @@ sudo "cd #{release_path}; rm -rf log"
 run "cd #{release_path};ln -sfv #{shared_path}/log ."
 run "cd #{release_path}/config;mkdir thinkingsphinx"
 if ['app_master', 'solo'].include?(node[:instance_role])
-  run "cd #{release_path} pkill -f 'searchd' && bundle exec rake thinking_sphinx:configure && bundle exec rake thinking_sphinx:running_start"
+  run "cd #{release_path} && pkill -f 'searchd' && bundle exec rake thinking_sphinx:configure && bundle exec rake thinking_sphinx:running_start"
   sudo "monit monitor sphinx_#{appname}_#{appname == 'auxbonnesadresses' ? 3312 : 3313}"
 else
   run "cd #{release_path} bundle exec rake thinking_sphinx:configure"
